8bf74a6cf603dbfd7f9def0b33f13e8610938ec5
Vinamra Baghel <thevinamrabaghel@gmail.com>
2025-06-09 15:52:22 -0400

    Backup upto task 5

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyze):
+    url = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
+    headers = {
+        "Content-Type": "application/json",
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    input_text = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=input_text)
+    response.raise_for_status()
+
+    result = json.loads(response.text)
+    scores = result['emotionPredictions'][0]['emotion']
+    scores['dominant_emotion'] = max(scores, key = scores.get)
+    
+    return scores
+
+if __name__ == "__main__":
+    print(emotion_detector("I love this new technology."))
